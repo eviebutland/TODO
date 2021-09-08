41e5a9f301615a1a384fa2eed3bdc474793f9f87
Evie Butland <eviebutland@Evies-MacBook-Air.local>
2021-09-08 16:10:34 +0100

    adding more styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <nav>
-      <NuxtLink to="/">Home</NuxtLink>
+    <nav class="w-full text-gray-700  h-16  animated z-40 flex md:flex-start shadow  md:items-center mb-10">
+      <ul>
+        <NuxtLink to="/" class="text-xl text-gray-900 leading-1 hover:text-gray-400 mx-4 md:my-0">Home</NuxtLink>
+        <NuxtLink to="/addToDo" class="text-xl text-gray-900 leading-1 hover:text-gray-400 mx-4 md:my-0">Add</NuxtLink>
+        <NuxtLink to="/addToDo" class="text-xl text-gray-900 leading-1 hover:text-gray-400 mx-4 md:my-0">Filter</NuxtLink>
+      </ul>
     </nav>
     <Nuxt/>
-    <footer>
-      <p>Footer</p>
-      <NuxtLink to="/">Home</NuxtLink>
+    <footer class="shadow h-16 md:items-center mt-10">
+      <ul>
+        <NuxtLink to="/" class="text-sm leading-1 hover:text-gray-400 mx-4 md:my-0 text-indigo-500">Home</NuxtLink>
+        <NuxtLink to="/addToDo" class="text-sm text-indigo-500 leading-1 hover:text-gray-400 mx-4 md:my-0">Add</NuxtLink>
+        <NuxtLink to="/addToDo" class="text-sm text-indigo-500 leading-1 hover:text-gray-400 mx-4 md:my-0">Filter</NuxtLink>
+      </ul>
     </footer>
   </div>
 </template>
---
+++ pages/filter.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>filter by category, status, time etc</h1>
+</template>
