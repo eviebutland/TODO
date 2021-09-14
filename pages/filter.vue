bbd7a8b75764993f5154cf8b1db6509487be83b1
Evie Butland <eviebutland@Evies-MacBook-Air.local>
2021-09-14 17:13:57 +0100

    building on filter page

--- pages/filter.vue
+++ pages/filter.vue
@@ -1,3 +1,67 @@
 <template>
-  <h1>filter by category, status, time etc</h1>
+  <section>
+    <h1>filter by category, status, time etc</h1>
+    <div>
+      <form>
+        <label for="filterTodo">Filter</label>
+        <select name="filterTodo" v-model="filterBy">
+          <option value="category">Category</option>
+          <option value="status">Status</option>
+          <option value="group">Group</option>
+        </select>
+        <div v-if="filterBy === 'category'">
+          <label for="filterCategory">Category</label>
+          <select name="filterTodo" v-model="category" v-on:change="displayCategory">
+            <option value="quick-task">Quick task</option>
+            <option value="task">Task</option>
+            <option value="project">Project</option>
+          </select>
+        </div>
+        <div v-if="filterBy === 'status'">
+          <label for="filterCategory">Status</label>
+          <select name="filterTodo" v-model="status" v-on:change="filterByStatus(status)">
+            <option value="complete">Complete</option>
+            <option value="not-complete">Not Complete</option>
+          </select>
+        </div>
+        <div v-if="filterBy === 'group'">
+          <label for="filterCategory">Group</label>
+          <select name="filterTodo" v-model="group" v-on:change="filterByGroup(group)">
+            <option value="work">Work</option>
+            <option value="personal">Personal</option>
+          </select>
+        </div>
+      </form>
+      {{filterByCategory(category)}}
+      {{filterByGroup(group)}}
+    </div>
+  </section>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Filter',
+  data() {
+    return {
+      filterBy: '',
+      category: '',
+      status: '',
+      group: '',
+      list: []
+    }
+  },
+  methods: {
+    ...mapGetters([
+      'filterByCategory',
+      'filterByStatus',
+      'filterByGroup'
+    ]),
+    displayCategory() {
+      
+      this.filterByCategory(this.category)
+      console.log(this.filterByCategory(this.category))
+    }
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -21,12 +21,39 @@ export const getters = {
   numberOfTodos(state) {
     return state.todos.length
   },
-  filterByCompleted(){
-    // filter by completed tasks
-  },
   getSingleItem(state){
     return (id) => {
       return state.todos.find(todo => todo.id == id)
+    }
+  },
+  filterByCategory(state, category) {
+    switch(category) {
+      case 'quick-task':
+        return state.todos.filter(todo => todo.category === 'quick-task')
+      case 'task': 
+        return state.todos.filter(todo => todo.category === 'task')
+      case 'project':
+        return state.todos.filter(todo => todo.category === 'project')
+      default:
+        return []
+    }
+  },
+  filterByStatus(state, status) {
+    switch(status) {
+      case 'complete':
+        return state.todos.filter(todo => todo.status === 'complete')
+      case 'not-complete': 
+        return state.todos.filter(todo => todo.status === 'not-complete')
+      default:
+        return []
+    }
+  },
+  filterByGroup(state, group){
+    switch(group) {
+      case 'work': 
+        return state.todos.filter(todo => todo.group === 'work')
+      case 'personal':
+        return state.todos.filter(todo => todo.group === 'personal')
     }
   }
 }
